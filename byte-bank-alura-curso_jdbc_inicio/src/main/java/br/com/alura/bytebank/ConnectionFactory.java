634627c1ca1eaf0773bd04742562543798c4186b
andrey <andrey.damasceno@outlook.com>
2023-08-03 08:21:19 -0300

    correções

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
     public Connection recuperarConexao() {
 
         try {
-            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/kkk?user=root&password=@Ndrey123");
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/byte_bank?user=root&password=@Ndrey123");
         } catch (SQLException e) {
            throw new RuntimeException(e);
         }
