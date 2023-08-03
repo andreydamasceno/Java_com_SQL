7ef121865864841b8bab44cf7b7380fca0331494
andrey <andrey.damasceno@outlook.com>
2023-08-03 08:19:25 -0300

    Initial commit

---
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package br.com.alura.bytebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public Connection recuperarConexao() {
+
+        try {
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/kkk?user=root&password=@Ndrey123");
+        } catch (SQLException e) {
+           throw new RuntimeException(e);
+        }
+    }
+}
